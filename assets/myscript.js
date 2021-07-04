50229030a3609738dbe1b41405670f2aa6b18dfd
benny801205 <54221041+benny801205@users.noreply.github.com>
2021-07-04 03:46:40 -0600

    debug filter function

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,4 +1,25 @@
 
+// a hide set
+
+var hide_set =new Set()
+
+
+
+function isHide(string_list){
+	
+	string_list.foreach(function(item){
+		if(!hide_set.has(item)){
+			return false;
+		}
+	})
+	return true;
+	
+	
+	
+}
+
+
+
 $('.display-label')
   .on('click', function(){
 	let code = $(this).attr('value')
@@ -6,9 +27,12 @@ $('.display-label')
 	let target_label = '#hide-'+ code
 	$(target_label).show()
 	let target_project = '.project-'+ code
-	$(target_project).hide()
-    console.log(target_label)
-
+	
+	hide_set.add(code)
+	$(target_project).each(function(){
+		if (isHide($(this).attr('value').split(' '))){
+		$(this).hide()
+		}});
   })
 
 
@@ -19,6 +43,9 @@ $('.hide-label')
 	$(this).hide()
 	let target_label = '#display-'+ code
 	$(target_label).show()
+	
+	hide_set.delete(code)
+	
 	let target_project = '.project-'+ code
 	$(target_project).show()
     console.log(target_label)
