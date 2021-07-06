d3a8cd676b465e53c13883852965bb44f55adff5
benny801205 <54221041+benny801205@users.noreply.github.com>
2021-07-05 19:18:38 -0600

    update filter

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,12 +1,48 @@
 
 // a hide set
 
-var hide_set =new Set()
-const display_list= [
+const all_attribute =['java',
+	'python',
+	'csharp',
+	'ml',
+	'back-end',
+	'front-end',
+	'mobile-app'
+	]
+
+
+
+const prefer_attribute= [
 	"front-end",
 	"back-end"
-
 ];
+
+var hide_set =new Set(all_attribute)
+
+
+$('.project').hide()
+//prefer_attribute.forEach(code => display_prefer_project(code))
+
+
+
+
+
+function display_prefer_project(code){
+	
+	let this_label = "#hide-" + code
+	$(this_label).hide()
+	
+	hide_set.delete(code)
+	
+	let target_project = '.project-'+ code
+	$(target_project).show()
+    console.log(target_label)
+	
+	
+}
+
+
+
 
 
 function isHide(string_list){
@@ -16,9 +52,6 @@ function isHide(string_list){
 		}
 	}
 	return true;
-	
-	
-	
 }
 
 
